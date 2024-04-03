3b9fb235bd12a54b66f35882915143ff99acff33
Taistonalle <55252093+Taistonalle@users.noreply.github.com>
2024-04-03 15:56:17 +0300

    Imported Gabriella's assets & Easy Encapsulation
    Imported:
    Gabriella's background for the desktop
    Notepad window
    Terminal window
    
    Made a prototype kind of a thing about the Easy encapsulation task

--- HackerGame/Assets/Scripts/TaskScripts/DragUIElement.cs
+++ HackerGame/Assets/Scripts/TaskScripts/DragUIElement.cs
@@ -2,26 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
-public class CodeBlock_EE : MonoBehaviour, IDragHandler, IEndDragHandler {
-    [SerializeField] private int id;
-    public int Id {
-        get { return id; }
-        set { id = value; }
-    }
-    void Start() {
-
-    }
+public class DragUIElement : MonoBehaviour, IDragHandler, IEndDragHandler {
 
     /*Documentation about the drag handler Interface: https://docs.unity3d.com/2018.3/Documentation/ScriptReference/EventSystems.IDragHandler.html
       For some reason there is no documentation page for newer version of Unity
+
+      If you want to drag UI elements Inherit this script to other scripts :)
      */
     public void OnDrag(PointerEventData eventData) {
         transform.position = Input.mousePosition;
     }
 
-    public void OnEndDrag(PointerEventData eventData) {
+    /*
+     * Slapped some Polymorphism here: https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/object-oriented/polymorphism
+     */
+    public virtual void OnEndDrag(PointerEventData eventData) {
         Debug.Log($"Drag ended on {gameObject.name}");
     }
+
 }
---
+++ HackerGame/Assets/Scripts/TaskScripts/Easy Encapsulation/Task_EE.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Task_EE : MonoBehaviour {
+
+    [SerializeField] GameObject[] slots, blocks;
+    [SerializeField] TextMeshProUGUI terminalTxt;
+    [SerializeField] float messageTimeOnTerminal;
+
+    [Space(10f)]
+    [Header("Terminal messages")]
+    [SerializeField] string correctMessage;
+    [SerializeField] string wrongMessage;
+
+    //Call this function from button
+    public void CheckAnswer() {
+        bool correct = false;
+        for (int i = 0; i < slots.Length; i++) {
+            if (slots[i].transform.position != blocks[i].transform.position) {
+                correct = false;
+                break;
+            } 
+            else correct = true;
+        }
+
+        if (correct) StartCoroutine(TerminalMessage(correctMessage, true));
+        else if (!correct) {
+            foreach (GameObject block in blocks) block.GetComponent<CodeBlock_EE>().ResetBlockPos();
+            foreach (GameObject slot in slots) slot.SetActive(false);
+
+            StartCoroutine(TerminalMessage(wrongMessage, false));
+        }
+    }
+
+
+    IEnumerator TerminalMessage(string message, bool correct) {
+        //Store old message
+        string oldMessage = terminalTxt.text;
+
+        switch (correct) {
+            case true: 
+            terminalTxt.text = message;
+            Debug.Log("Implement unlocking for harder task");
+            foreach (GameObject slot in slots) slot.SetActive(false);
+            foreach (GameObject block in blocks) block.SetActive(false);
+            yield return new WaitForSeconds(messageTimeOnTerminal);
+            Destroy(gameObject);
+            break;
+            
+            case false: 
+            terminalTxt.text = message;
+            yield return new WaitForSeconds(messageTimeOnTerminal);
+            terminalTxt.text = oldMessage;
+            foreach (GameObject slot in slots) slot.SetActive(true);
+            break;
+        }
+    }
+}
