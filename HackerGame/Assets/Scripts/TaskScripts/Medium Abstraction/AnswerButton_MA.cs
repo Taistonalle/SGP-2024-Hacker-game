b8671f815e2c71fbf556eb5690339e62a678cbd9
WaltteriNupponen <waltteri.nupponen@edu.turkuamk.fi>
2024-04-24 15:04:42 +0300

    Medium Abstraction task done

---
+++ HackerGame/Assets/Scripts/TaskScripts/Medium Abstraction/AnswerButton_MA.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerButton_MA : AnswerButton_MI
+{
+
+}
--- HackerGame/Assets/Scripts/TaskScripts/Medium Abstraction/QuestionSetup_MA.cs
+++ HackerGame/Assets/Scripts/TaskScripts/Medium Abstraction/QuestionSetup_MA.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestionSetup_MP : QuestionSetup_MI
+public class QuestionSetup_MA : QuestionSetup_MI
 {
     // Overrides the location the questions are fetch
     public override void GetQuestionAssets()
     {
         // Get all of the questions from the questions folder
-        questions = new List<QuestionData_MI>(Resources.LoadAll<QuestionData_MI>("Questions_MP"));
+        questions = new List<QuestionData_MI>(Resources.LoadAll<QuestionData_MI>("Questions_MA"));
     }
 }
