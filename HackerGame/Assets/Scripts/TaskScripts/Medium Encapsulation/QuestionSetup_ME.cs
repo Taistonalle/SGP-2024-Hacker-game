f28e799efd853ab42d49c1629645be8c107fc9b7
WaltteriNupponen <waltteri.nupponen@edu.turkuamk.fi>
2024-04-24 15:19:43 +0300

    Encapsulation medium task done

---
+++ HackerGame/Assets/Scripts/TaskScripts/Medium Encapsulation/QuestionSetup_ME.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionSetup_ME : QuestionSetup_MI
+{
+    // Overrides the location the questions are fetch
+    public override void GetQuestionAssets()
+    {
+        // Get all of the questions from the questions folder
+        questions = new List<QuestionData_MI>(Resources.LoadAll<QuestionData_MI>("Questions_ME"));
+    }
+}
