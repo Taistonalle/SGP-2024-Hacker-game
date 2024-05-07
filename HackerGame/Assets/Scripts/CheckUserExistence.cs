6cb6075db4d701e1dbf548a80f061e36a2a9fd18
Teemu Homi <53630914+ohmitek@users.noreply.github.com>
2024-05-07 10:17:48 +0300

    Username check to login screen
    Username check to login screen

---
+++ HackerGame/Assets/Scripts/CheckUserExistence.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using TMPro;
+
+public class CheckUserExistence : MonoBehaviour
+{
+    public TMP_InputField Username_Inputfield; // Assign this in the inspector
+
+    public void CheckUsername()
+    {
+        StartCoroutine(CheckUsernameCoroutine());
+    }
+
+    private IEnumerator CheckUsernameCoroutine()
+    {
+        string username = Username_Inputfield.text;
+        string url = $"http://admin:TOSISALAINENSALASANA@44.211.154.174:5984/playerdata/{username}";
+
+        UnityWebRequest getReq = UnityWebRequest.Get(url);
+        yield return getReq.SendWebRequest();
+
+        if (getReq.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Username doesn't exist");
+        }
+        else
+        {
+            Debug.Log("Username exists");
+        }
+    }
+}
