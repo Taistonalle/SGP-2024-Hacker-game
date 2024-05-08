26cbdb6ff74b3d727a29234f9a99ab72088193e0
Taistonalle <55252093+Taistonalle@users.noreply.github.com>
2024-05-08 18:41:47 +0300

    Pdf report modification
    Missing hard data, adding that later

--- HackerGame/Assets/Scripts/PlayerDataDisplay.cs
+++ HackerGame/Assets/Scripts/PlayerDataDisplay.cs
@@ -15,10 +15,20 @@ public class PlayerDataDisplay : MonoBehaviour
     public PlayerDataHandler playerDataHandler; // Reference to PlayerDataHandler
     //[SerializeField] Image notepad; //Teemu K addition
     readonly StringBuilder sb = new(); //Moved this here - Teemu K
+    string mainTitle, userNameAndEmail;
+    string EETitle, EEData, EATitle, EAData, EITitle, EIData, EPTitle, EPData;
+    string METitle, MEData, MATitle, MAData, MITitle, MIData, MPTitle, MPData;
+    string HETitle, HEData, HATitle, HAData, HITitle, HIData, HPTitle, HPData;
 
+    int task_EE_Attempts, task_EA_Attempts, task_EI_Attempts, task_EP_Attempts;
+    int task_ME_Attempts, task_MA_Attempts, task_MI_Attempts, task_MP_Attempts;
+    int task_HE_Attempts, task_HA_Attempts, task_HI_Attempts, task_HP_Attempts;
+    int allAttempts;
 
     private void Start()
     {
+        
+
         if (playerDataHandler == null || playerDataHandler.currentPlayerData == null || displayText == null)
         {
             Debug.LogError("PlayerDataHandler or displayText is not set. Please set them in the Inspector.");
@@ -26,6 +36,43 @@ public class PlayerDataDisplay : MonoBehaviour
         }
 
         playerDataHandler = FindObjectOfType<PlayerDataHandler>();
+
+        //Define strings
+        mainTitle = "Player data";
+        userNameAndEmail = $"\nEmail: {playerDataHandler.currentPlayerData.email}\nUsername: {playerDataHandler.currentPlayerData.userName}";
+        EETitle = "\nTask EE Data:";
+        EATitle = "\nTask EA Data:";
+        EITitle = "\nTask EI Data:";
+        EPTitle = "\nTask EP Data:";
+        METitle = "\nTask ME Data:";
+        MATitle = "\nTask MA Data:";
+        MITitle = "\nTask MI Data:";
+        MPTitle = "\nTask MP Data:";
+        HETitle = "\nTask HE Data:";
+        HATitle = "\nTask HA Data:";
+        HITitle = "\nTask HI Data:";
+        HPTitle = "\nTask HP Data:";
+
+        //Define attempts
+        task_EE_Attempts = playerDataHandler.currentPlayerData.task_EE_data.Count;
+        task_EA_Attempts = playerDataHandler.currentPlayerData.task_EA_data.Count;
+        task_EI_Attempts = playerDataHandler.currentPlayerData.task_EI_data.Count;
+        task_EP_Attempts = playerDataHandler.currentPlayerData.task_EP_data.Count;
+
+        task_ME_Attempts = playerDataHandler.currentPlayerData.task_ME_data.Count;
+        task_MA_Attempts = playerDataHandler.currentPlayerData.task_MA_data.Count;
+        task_MI_Attempts = playerDataHandler.currentPlayerData.task_MI_data.Count;
+        task_MP_Attempts = playerDataHandler.currentPlayerData.task_MP_data.Count;
+
+        task_HE_Attempts = playerDataHandler.currentPlayerData.task_HE_data.Count;
+        task_HA_Attempts = playerDataHandler.currentPlayerData.task_HA_data.Count;
+        task_HI_Attempts = playerDataHandler.currentPlayerData.task_HI_data.Count;
+        task_HP_Attempts = playerDataHandler.currentPlayerData.task_HP_data.Count;
+
+        allAttempts = task_EE_Attempts + task_EA_Attempts + task_EI_Attempts + task_EP_Attempts +
+                          task_ME_Attempts + task_MA_Attempts + task_MI_Attempts + task_MP_Attempts +
+                          task_HE_Attempts + task_HA_Attempts + task_HI_Attempts + task_HP_Attempts;
+
         ShowPlayerData();
     }
 
@@ -39,13 +86,64 @@ public class PlayerDataDisplay : MonoBehaviour
         // Use a StringBuilder for efficient string manipulation
         //StringBuilder sb = new StringBuilder();
 
+        //Add player information
+        sb.AppendLine(mainTitle);
+        sb.AppendLine(userNameAndEmail);
+        sb.AppendLine($"Task EE attempts: {task_EE_Attempts}");
+        sb.AppendLine($"Task EA attempts: {task_EA_Attempts}");
+        sb.AppendLine($"Task EI attempts: {task_EI_Attempts}");
+        sb.AppendLine($"Task EP attempts: {task_EP_Attempts}");
+        sb.AppendLine($"Task ME attempts: {task_ME_Attempts}");
+        sb.AppendLine($"Task MA attempts: {task_MA_Attempts}");
+        sb.AppendLine($"Task MI attempts: {task_MI_Attempts}");
+        sb.AppendLine($"Task MP attempts: {task_MP_Attempts}");
+        sb.AppendLine($"Task HE attempts: {task_HE_Attempts}");
+        sb.AppendLine($"Task HA attempts: {task_HA_Attempts}");
+        sb.AppendLine($"Task HI attempts: {task_HI_Attempts}");
+        sb.AppendLine($"Task HP attempts: {task_HP_Attempts}");
+        sb.AppendLine($"All attempts: {allAttempts}");
+
+        //Add Task EE data
+        sb.AppendLine(EETitle);
+        DisplayTaskData(sb, playerData.task_EE_data, 1);
+
+        //Add Task EA data
+        sb.AppendLine(EATitle);
+        DisplayTaskData(sb, playerData.task_EA_data, 2);
+
+        //Add Task EI data
+        sb.AppendLine(EITitle);
+        DisplayTaskData(sb, playerData.task_EI_data, 3);
+
+        //Add Task EP data
+        sb.AppendLine(EPTitle);
+        DisplayTaskData(sb, playerData.task_EP_data, 4);
+
+        //Add Task ME data
+        sb.AppendLine(METitle);
+        DisplayTaskData(sb, playerData.task_ME_data, 1);
+
+        //Add Task MA data
+        sb.AppendLine(MATitle);
+        DisplayTaskData(sb, playerData.task_MA_data, 2);
+
+        //Add Task MI data
+        sb.AppendLine(MITitle);
+        DisplayTaskData(sb, playerData.task_MI_data, 3);
+
+        //Add Task MP data
+        sb.AppendLine(MPTitle);
+        DisplayTaskData(sb, playerData.task_MP_data, 4);
+
+        /* Old data information
         // Add basic information
         sb.AppendLine("Player Data:");
         sb.AppendLine();
         sb.AppendLine($"Email: {playerData.email}");
         sb.AppendLine($"Username: {playerData.userName}");
+        */
 
-
+        /* Old data adding
         // Add task_EE_data
         sb.AppendLine("\nTask EE Data:");
         DisplayTaskData(sb, playerData.task_EE_data);
@@ -62,40 +160,92 @@ public class PlayerDataDisplay : MonoBehaviour
         sb.AppendLine("\nTask EP Data:");
         DisplayTaskData(sb, playerData.task_EP_data);
 
+        // Add task_ME_data
+        sb.AppendLine("\nTask ME Data:");
+        DisplayTaskData(sb, playerData.task_ME_data);
+
+        // Add task_MA_data
+        sb.AppendLine("\nTask MA Data:");
+        DisplayTaskData(sb, playerData.task_MA_data);
+
+        // Add task_MI_data
+        sb.AppendLine("\nTask MI Data:");
+        DisplayTaskData(sb, playerData.task_MI_data);
+
+        // Add task_MP_data
+        sb.AppendLine("\nTask MP Data:");
+        DisplayTaskData(sb, playerData.task_MP_data);
+        */
         // Set the Text component to display the formatted data
         displayText.text = sb.ToString();
     }
 
-    private void DisplayTaskData(StringBuilder sb, List<Easy_Task_Data> taskDataList)
+    private void DisplayTaskData(StringBuilder sb, List<Easy_Task_Data> taskDataList, int dataNum)
     {
         for (int i = 0; i < taskDataList.Count; i++)
         {
-            string attemptData = $"Attempt: {taskDataList[i].attempt}" +
-                                $"\nCorrect amount: {taskDataList[i].correctAmount}";
+            string attemptData = $"\nAttempt: {taskDataList[i].attempt}" +
+                          $"\nCorrect amount: {taskDataList[i].correctAmount}";
 
             // In the current task data, loop through its slot data array
             foreach (Slot_Data slot in taskDataList[i].slot_datas)
             {
                 attemptData += $"\nSlot name: {slot.slotName}" +
-                            $"\nDescription: {slot.description}" +
-                            $"\nWas correct: {slot.wasCorrect}";
+                               $"\nDescription: {slot.description}" +
+                               $"\nWas correct: {slot.wasCorrect}\n";
             }
             sb.AppendLine(attemptData);
-            sb.AppendLine(); // Empty line after each attempt
+            //sb.AppendLine(); // Empty line after each attempt
+
+            switch (dataNum) {
+                case 1:
+                EEData += attemptData;
+                break;
+
+                case 2:
+                EAData += attemptData;
+                break;
+
+                case 3:
+                EIData += attemptData;
+                break;
+
+                case 4:
+                EPData += attemptData;
+                break;
+            }
         }
     }
 
-    private void DisplayTaskData(StringBuilder sb, List<Medium_Task_Data> taskDataList) {
+    private void DisplayTaskData(StringBuilder sb, List<Medium_Task_Data> taskDataList, int dataNum) {
         for (int i = 0; i < taskDataList.Count;i++) {
-            string attemptData = $"Attempt: {taskDataList[i].attempt}" +
+            string attemptData = $"\nAttempt: {taskDataList[i].attempt}" +
                                  $"\nCorrect amount: {taskDataList[i].correctAmount}";
 
             foreach (QuestionSelectionData question in taskDataList[i].questionData) {
                 attemptData += $"\nQuestion: {question.question}" +
                                $"\nWhat was selected: {question.whatWasSelected}" +
-                               $"\nWas correct: {question.wasCorrect}";
+                               $"\nWas correct: {question.wasCorrect}\n";
             }
+            sb.AppendLine(attemptData);
 
+            switch (dataNum) {
+                case 1:
+                MEData += attemptData;
+                break;
+
+                case 2:
+                MAData += attemptData;
+                break;
+
+                case 3:
+                MIData += attemptData;
+                break;
+
+                case 4:
+                MPData += attemptData;
+                break;
+            }
         }
     }
 
@@ -220,14 +370,68 @@ public class PlayerDataDisplay : MonoBehaviour
         }
 
         // Create a PdfWriter instance to write the document to the specified file
-        PdfWriter.GetInstance(document, new FileStream(filePath, FileMode.Create));
+        PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(filePath, FileMode.Create));
 
         // Open the document for writing
         document.Open();
 
         // Add content to the document
-        document.Add(new Paragraph(sb.ToString(), FontFactory.GetFont(FontFactory.HELVETICA, 18)));
+        //document.Add(new Paragraph(sb.ToString(), FontFactory.GetFont(FontFactory.HELVETICA, 18))); Old, using the whole string builder. Cant modify headers this way
+        document.Add(new Paragraph(mainTitle, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 24, BaseColor.BLACK)));
+        document.Add(new Paragraph(userNameAndEmail, FontFactory.GetFont(FontFactory.HELVETICA, 14)));
+        document.Add(new Paragraph($"Task EE attempts: {task_EE_Attempts}" +
+                                   $"\nTask EA attempts: {task_EA_Attempts}" +
+                                   $"\nTask EI attempts: {task_EI_Attempts}" +
+                                   $"\nTask EP attempts: {task_EP_Attempts}" +
+                                   $"\nTask ME attempts: {task_ME_Attempts}" +
+                                   $"\nTask MA attempts: {task_MA_Attempts}" +
+                                   $"\nTask MI attempts: {task_MI_Attempts}" +
+                                   $"\nTask MP attempts: {task_MP_Attempts}" +
+                                   $"\nTask HE attempts: {task_HE_Attempts}" +
+                                   $"\nTask HA attempts: {task_HA_Attempts}" +
+                                   $"\nTask HI attempts: {task_HI_Attempts}" +
+                                   $"\nTask HP attempts: {task_HP_Attempts}" +
+                                   $"\nAll attempts: {allAttempts}", FontFactory.GetFont(FontFactory.HELVETICA, 14)));
 
+        document.Add(new Paragraph(EETitle, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 16, new BaseColor(30, 115, 45))));
+        document.Add(new Paragraph(EEData, FontFactory.GetFont(FontFactory.HELVETICA, 14)));
+
+        document.Add(new Paragraph(EATitle, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 16, new BaseColor(30, 115, 45))));
+        document.Add(new Paragraph(EAData, FontFactory.GetFont(FontFactory.HELVETICA, 14)));
+
+        document.Add(new Paragraph(EITitle, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 16, new BaseColor(30, 115, 45))));
+        document.Add(new Paragraph(EIData, FontFactory.GetFont(FontFactory.HELVETICA, 14)));
+
+        document.Add(new Paragraph(EPTitle, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 16, new BaseColor(30, 115, 45))));
+        document.Add(new Paragraph(EPData, FontFactory.GetFont(FontFactory.HELVETICA, 14)));
+
+        document.Add(new Paragraph(METitle, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 16, new BaseColor(110, 110, 50))));
+        document.Add(new Paragraph(MEData, FontFactory.GetFont(FontFactory.HELVETICA, 14)));
+
+        document.Add(new Paragraph(MATitle, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 16, new BaseColor(110, 110, 50))));
+        document.Add(new Paragraph(MAData, FontFactory.GetFont(FontFactory.HELVETICA, 14)));
+
+        document.Add(new Paragraph(MITitle, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 16, new BaseColor(110, 110, 50))));
+        document.Add(new Paragraph(MIData, FontFactory.GetFont(FontFactory.HELVETICA, 14)));
+
+        document.Add(new Paragraph(MPTitle, FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 16, new BaseColor(110, 110, 50))));
+        document.Add(new Paragraph(MPData, FontFactory.GetFont(FontFactory.HELVETICA, 14)));
+
+        /* Doesn't work.. yet at least.
+        // Set background color for the whole document
+        Rectangle pageSize;
+        PdfContentByte canvas;
+        int totalPages = document.PageNumber;
+        List<Rectangle> pages = new List<Rectangle>();
+        for (int i = 0; i <= document.PageNumber; i++) {
+            pageSize = document.PageSize;
+            canvas = writer.DirectContentUnder;
+            canvas.SetColorFill(new BaseColor(30, 30, 30));
+            canvas.Rectangle(pageSize.Left, pageSize.Bottom, pageSize.Width, pageSize.Height);
+            document.NewPage(); // Move to the next page
+            canvas.Fill();
+        }
+        */
         // Close the document
         document.Close();
         //----End of pdf---
